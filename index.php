2273fc78c1450a403d8e3d8fdb123453dfbc38ef
syayidabdan <syayidabdann@gmail.com>
2023-11-23 08:03:30 +0700

    commit kedua

--- index.php
+++ index.php
@@ -31,19 +31,15 @@ $nomor= 1;
         </svg>
               Catatan Gw
         </h3>
-        <div class="card shadow bg-body-tertiary rounded mb-4 ">
+        <div class="mb-3">
             <div class="card-body">
-                <form class="d-flex" action="insert.php" method="post">
-                    <input class="form-control me-2" required name="title_note" placeholder="JUDUL...">
-                    <input class="form-control me-2" required name="note_note" placeholder="ISINYA.......">
-                    <button class="btn btn-primary" type="submit">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-clipboard-plus" viewBox="0 0 16 16">
+                <a href="add.php" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-clipboard-plus" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z"/>
                             <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
                             <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
                           </svg>
-                    </button>
-                </form>
+                          Tambahkan Catatan
+                  </a>
             </div>
         </div>
               <?php
